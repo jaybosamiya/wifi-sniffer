07b865c04962e1fa3ad5f48e072a81235499a80a
Jay Bosamiya <jaybosamiya@gmail.com>
2015-03-07 16:09:26 +0530

    Add utilities

---
+++ util.cpp
@@ -0,0 +1,46 @@
+#include "util.h"
+
+#include <iostream>
+#include <cstdarg>
+#include <cstdio>
+
+using namespace std;
+
+static int verbose_flag = 0;
+static int debug_flag = 0;
+
+const int BUFSIZE(200);
+static char TEMPOUT[BUFSIZE];
+
+void set_verbose_on() {
+  verbose_flag = 1;
+}
+
+void set_debug_on() {
+  debug_flag = 1;
+}
+
+void verbose(char * fmt, ...) {
+  va_list argp;
+
+  va_start(argp, fmt);
+  vsnprintf(TEMPOUT, sizeof(TEMPOUT), fmt, argp);
+  va_end(argp);
+
+  if ( verbose_flag ) {
+    cerr << TEMPOUT << endl;
+  }
+}
+
+void debug(char * fmt, ...) {
+  va_list argp;
+
+  va_start(argp, fmt);
+  vsnprintf(TEMPOUT, sizeof(TEMPOUT), fmt, argp);
+  va_end(argp);
+
+  if ( debug_flag ) {
+    cerr << TEMPOUT << endl;
+  }
+}
+
---
+++ util.h
@@ -0,0 +1,10 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+void set_verbose_on();
+void set_debug_on();
+
+void verbose(char * fmt, ...);
+void debug(char * fmt, ...);
+
+#endif

ae0cbb97f56145b874dc504bf93c89dd7c9a5283
Jay Bosamiya <jaybosamiya@gmail.com>
2015-03-10 14:23:20 +0530

    Do some sniffing

--- sniffer.cpp
+++ sniffer.cpp
@@ -6,11 +6,29 @@
 #include <ctime>
 #include <string>
 #include <pcap.h>
+#include <map>
 
 using namespace std;
 
 static pcap_t *handle = NULL;
 static int datalink;
+char * interface;
+
+const int num_channels = 12;
+int current_channel = 0;
+
+void set_monitor_mode(char * iface) {
+  interface = iface;
+  char * const argv[] = {(char*)("iwconfig"),iface,(char*)("mode"),(char*)("monitor"),0};
+  run_command(argv);
+}
+
+const float max_time = 10;
+const float round_time = 1;
+
+float channel_prob[num_channels+1];
+int channel_time[num_channels+1];
+int channel_packets[num_channels+1];
 
 void initialize(char * interface) {
   if ( handle ) {
@@ -19,6 +37,8 @@ void initialize(char * interface) {
   }
 
   char errbuf[BUFSIZ];
+
+  set_monitor_mode(interface);
 
   handle = pcap_open_live(interface, BUFSIZ, 1, 1000, errbuf);
   if (handle == NULL) {
@@ -61,6 +81,8 @@ struct prism_header{
   prism_value frmlen;
 };
 
+map<string,int> mac_count[num_channels+1][4];
+
 void handleMAC(const u_char * mac, int pos) {
   char mac_c_str[13];
   mac_c_str[0] = 0;
@@ -68,8 +90,8 @@ void handleMAC(const u_char * mac, int pos) {
     sprintf(mac_c_str,"%s%02X",mac_c_str,mac[i]);
   }
   string mac_str(mac_c_str);
+  mac_count[current_channel][pos][mac_str]++;
   debug("MAC %d : %s",pos,mac_c_str);
-  // TODO: Add to the buckets
 }
 
 void handlePacket(const u_char* packet) {
@@ -83,6 +105,8 @@ void handlePacket(const u_char* packet) {
   for ( int i = 0 ; i < 4 ; i++ ) {
     handleMAC(packet+4+(i*6),i);
   }
+
+  channel_packets[current_channel]++;
 }
 
 class Timer {
@@ -95,12 +119,56 @@ public:
   Timer() { reset(); }
 };
 
-const float max_time = 10;
+void change_channel(int channel) {
+  if ( channel < 1 || channel > num_channels ) {
+    error("Impossible to switch to channel %d. Quitting.",channel);
+    abort();
+  }
+  current_channel = channel;
+  char channel_no[3];
+  sprintf(channel_no,"%d",channel);
+  char * const argv[] = {(char*)"iwconfig",interface,(char*)"channel",channel_no,0};
+  run_command(argv);
+}
+
+bool switch_to_next_channel() {
+  bool ret = (current_channel==num_channels);
+  static Timer ch_time;
+  channel_time[current_channel]+=ch_time.get_time();
+  change_channel((current_channel % num_channels) + 1);
+  ch_time.reset();
+  return ret;
+}
+
+void recalculate_probs() {
+  const float min_speed_adder = 0.01;
+  float speed[num_channels+1];
+  float total_speed = 0;
+  for ( int i = 1 ; i <= num_channels ; i++ ) {
+    debug("Packets on channel %02d = %d",i,channel_packets);
+    speed[i] = channel_packets[i]/channel_time[i];
+    speed[i] += min_speed_adder;
+    total_speed += speed[i];
+  }
+
+  for ( int i = 1 ; i <= num_channels ; i++ ) {
+    channel_prob[i] = speed[i] / total_speed;
+  }
+}
 
 void capture_packets() {
-  static Timer timer;
+  Timer timer;
+  Timer channel_timer;
   while ( timer.get_time() < max_time ) {
     pcap_pkthdr header;
     handlePacket(pcap_next(handle, &header));
+    bool is_round_over;
+    if ( channel_timer.get_time() > channel_prob[current_channel] * round_time ) {
+      is_round_over = switch_to_next_channel();
+      channel_timer.reset();
+    }
+    if ( is_round_over ) {
+      recalculate_probs();
+    }
   }
 }
